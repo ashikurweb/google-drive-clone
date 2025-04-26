747d6e2d0d61b8b7d2b0cd53e6be45898619e5c9
ashikurweb <ashikurrahman7194@gmail.com>
2025-04-26 11:18:37 +0600

    Complete: StoreFolderReuest Logic System

--- app/Http/Requests/StoreFolderRequest.php
+++ app/Http/Requests/StoreFolderRequest.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Requests;
 
+use App\Models\File;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class StoreFolderRequest extends ParentIdBaseRequest
 {
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -14,8 +16,23 @@ class StoreFolderRequest extends ParentIdBaseRequest
      */
     public function rules(): array
     {
-        return array_merge(parent::rules(), [
-            'name' => 'required',
-        ], $this->getRules());
+        return array_merge(parent::rules(), 
+            [
+                'name' => [
+                    'required',
+                    Rule::unique(File::class, 'name')
+                        ->where('created_at', Auth::id())
+                        ->where('parent_id', $this->parent_id)
+                        ->whereNull('deleted_at')
+                ]
+            ]
+        );
+    }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => 'Folder: ":input" already exists',
+        ];
     }
 }
